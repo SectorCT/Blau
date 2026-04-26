7d1e7b779ff32ed2c2f5b5f08b105da188a818c3
dr-programmer <drtopuzov@gmail.com>
2026-04-26 10:57:22 +0300

    chore: host installer on GitHub Releases, gitignore downloads dir
    File exceeded GitHub's 100 MB limit. Moved to
    github.com/SectorCT/Blau/releases/download/v1.0.0/blau-setup.exe
    and excluded landing/public/downloads/ from git tracking.

--- landing/src/constants.ts
+++ landing/src/constants.ts
@@ -1,4 +1,4 @@
 export const REPO_URL = 'https://github.com/SectorCT/Blau'
 export const DEVPOST_URL = 'https://devpost.com/software/blau'
 export const DEMO_URL = '#'
-export const DOWNLOAD_URL = '/downloads/blau-setup.exe'
+export const DOWNLOAD_URL = 'https://github.com/SectorCT/Blau/releases/download/v1.0.0/blau-setup.exe'
